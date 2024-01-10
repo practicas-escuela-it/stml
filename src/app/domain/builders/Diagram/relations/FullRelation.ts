49e1be0a772823ea73802f236a3f9ce8cc17ee6a
G78405210 <apadron@atlantistecnologia.com>
2024-01-10 20:48:41 +0000

    refactor

--- src/app/domain/builders/Diagram/relations/FullRelation.ts
+++ src/app/domain/builders/Diagram/relations/FullRelation.ts
@@ -2,14 +2,8 @@ import { Model } from "src/app/domain/entities/Model";
 import { Relation } from "./Relation";
 import { Class } from "src/app/domain/entities/Class";
 import { ActionType } from "../ActionType";
-import { EfferentAssociationRelation } from "./EfferentAssociationRelation";
-import { EfferentCompositionRelation } from "./EfferentCompositionRelation";
-import { EfferentUseRelation } from "./EfferentUseRelation";
-import { EfferentInheritRelation } from "./EfferentInheritRelation";
-import { AfferentAssociationRelation } from "./AfferentAssociationRelation";
-import { AfferentCompositionRelation } from "./AfferentCompositionRelation";
-import { AfferentUseRelation } from "./AfferentUseRelation";
-import { AfferentInheritRelation } from "./AfferentInheritRelation";
+import { EfferentRelation } from "./EfferentRelation";
+import { AfferentRelation } from "./AfferentRelation";
 
 export class FullRelation extends Relation {
 
@@ -26,16 +20,10 @@ export class FullRelation extends Relation {
   }
 
   private _applyEfferentRelationClases(): void {
-      new EfferentAssociationRelation(this._modelClass, this._diagramClass, this._actionType, this._model, this._diagramModel).applyRelation();
-      new EfferentCompositionRelation(this._modelClass, this._diagramClass, this._actionType, this._model, this._diagramModel).applyRelation();
-      new EfferentUseRelation(this._modelClass, this._diagramClass, this._actionType, this._model, this._diagramModel).applyRelation();
-      new EfferentInheritRelation(this._modelClass, this._diagramClass, this._actionType, this._model, this._diagramModel).applyRelation();
+      new EfferentRelation(this._modelClass, this._diagramClass, this._actionType, this._model, this._diagramModel).applyRelation();
   }
 
   private _applyAfferentRelationClasses(): void {
-      new AfferentAssociationRelation(this._modelClass, this._model, this._diagramClass, this._actionType, this._diagramModel).applyRelation();
-      new AfferentCompositionRelation(this._modelClass, this._model, this._diagramClass, this._actionType, this._diagramModel).applyRelation();
-      new AfferentUseRelation(this._modelClass, this._model, this._diagramClass, this._actionType, this._diagramModel).applyRelation();
-      new AfferentInheritRelation(this._modelClass, this._model, this._diagramClass, this._actionType, this._diagramModel).applyRelation();
+      new AfferentRelation(this._modelClass, this._model, this._diagramClass, this._actionType, this._diagramModel).applyRelation();
   }
 }
---
+++ src/app/domain/entities/package.ts
@@ -0,0 +1,9 @@
+import { Class } from "./Class";
+
+export class Package {
+  private _classes: Class[];
+
+  constructor() {
+    this._classes = [];
+  }
+}
